4f6d375dc4715627890fa116ee240f715dd2d377
kos00pas <cpashi02@ucy.ac.cy>
2024-09-16 12:05:54 +0300

    first_toc

--- Test/test_318/src/Main.java
+++ Test/test_318/src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Person person_1 = new Person("KOKIS");
-        System.out.println(person_1.getName());
     }
 }
