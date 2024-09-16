d62b8bf9c0f3d3181c0dced5b89fd0c43e5fecea
kos00pas <cpashi02@ucy.ac.cy>
2024-09-16 12:10:00 +0300

    set up directories

--- Documentaions_project/First_test/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Person person_1 = new Person("KOKIS");
-        System.out.println(person_1.getName());
-    }
-}
